5bad05de0f67bda0e3aef694efad80bf69ebf590
Olha Livitchuk <olha.livitchuk@spryker.com>
2024-08-07 23:02:00 +0200

    FRW-8618 Fixes

--- _register.php
+++ _register.php
@@ -17,13 +17,4 @@ if (extension_loaded('opentelemetry') === false) {
 
 SprykerInstrumentationBootstrap::register();
 
-$autoload = function (string $class): void {
-    $convertedClassName = str_replace('\\', '-', $class);
-    $hookFilePath = sprintf(__DIR__ . '/src/Generated/OpenTelemetry/Hooks/%s/Hook.php', $convertedClassName);
 
-    if (getenv('OTEL_SDK_DISABLED') !== true && file_exists($hookFilePath)) {
-        include_once $hookFilePath;
-    }
-};
-
-spl_autoload_register($autoload, true, true);
